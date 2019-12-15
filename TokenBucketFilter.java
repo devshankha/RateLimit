0b653a82eceab6e282a63928739e76b0335c72d2
devshankha <45039485+devshankha@users.noreply.github.com>
2019-12-15 10:04:10 +0530

    Update TokenBucketFilter.java

--- TokenBucketFilter.java
+++ TokenBucketFilter.java
@@ -1,5 +1,15 @@
 import java.util.HashSet;
 import java.util.Set;
+/*
+ * Imagine you have a bucket that gets filled with tokens at the rate of 1 token per second. 
+ * The bucket can hold a maximum of N tokens. Implement a thread-safe class that lets threads
+ *  get a token when one is available. If no token is available, then the token-requesting
+ *   threads should block.
+
+The class should expose an API called getToken that various threads can call to get
+ a token
+ */
+
 
 public class TokenBucketFilter {
 	private int MAX_TOKENS;
